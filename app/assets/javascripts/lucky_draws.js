82108cba5a78157f62a0b6339456549c70bb3f24
adjidedjo <adji.el.djo@gmail.com>
2019-09-27 11:17:22 +0700

    fix date picker

---
+++ app/assets/javascripts/lucky_draws.js
@@ -0,0 +1,10 @@
+// Place all the behaviors and hooks related to the matching controller here.
+
+//--- Chosen
+//= require chosen_v1.2.0/chosen.jquery.min
+//--- Moment
+//= require moment/min/moment-with-locales.min
+//--- DatetimePicker
+//= require eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min
+// --- Select2
+//= require select2/dist/js/select2
