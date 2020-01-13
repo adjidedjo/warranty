0b119d66f702dc0e17e5f47e3628b0ce35f1e9b7
adjidedjo <adji.el.djo@gmail.com>
2020-01-13 14:07:16 +0700

    revise get travel voucher

--- app/models/lucky_gift.rb
+++ app/models/lucky_gift.rb
@@ -1,21 +1,4 @@
 class LuckyGift < ApplicationRecord
-  def self.get_value_vouchers(voucher)
-    kode = []
-    nilai = []
-    list_voucher = []
-    tvoucher = find_by_sql("SELECT * FROM traveloka_vouchers WHERE used = false ORDER BY CAST(nilai AS DECIMAL) DESC")
-    list_voucher << tvoucher.first.nilai.to_i
-    kode << tvoucher.first.kode
-
-    for e in tvoucher
-      a = list_voucher
-      b = kode
-      next e if a.sum > voucher.to_i || (a.sum + e.nilai.to_i) > voucher.to_i || kode.include?(e.kode)
-      list_voucher << e.nilai.to_i
-      kode << e.kode
-    end
-    return kode
-  end
 
   def self.get_traveloka_vouchers(voucher)
     voucher_value = find_by_sql("SELECT * FROM traveloka_vouchers WHERE nilai = '#{voucher}' AND used = false").first
@@ -23,7 +6,7 @@ class LuckyGift < ApplicationRecord
       value = get_value_vouchers(voucher)
       hasil = []
       value.each do |a|
-        voucher = find_by_sql("SELECT * FROM traveloka_vouchers WHERE kode = '#{a}'").first
+        voucher = find_by_sql("SELECT * FROM traveloka_vouchers WHERE nilai = #{a} AND used = false").first
   
         hasil << voucher
         update_traveloka_voucher(voucher)
@@ -33,6 +16,21 @@ class LuckyGift < ApplicationRecord
       update_traveloka_voucher(voucher_value)
       return voucher_value
     end
+  end
+
+  def self.get_value_vouchers(voucher)
+    nilai = 0
+    total = voucher.to_i - nilai.to_i
+    list_nilai = []
+    tvoucher = find_by_sql("SELECT * FROM traveloka_vouchers WHERE nilai <= #{voucher} AND used = false ORDER BY CAST(nilai AS DECIMAL) DESC")
+
+    for e in tvoucher
+      if (nilai+e.nilai.to_i) <= voucher.to_i
+        list_nilai << e.nilai.to_i
+      end
+      nilai = list_nilai.sum
+    end
+    return list_nilai
   end
 
   def self.update_traveloka_voucher(voucher)
