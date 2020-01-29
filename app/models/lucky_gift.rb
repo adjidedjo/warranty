256d1164aef782658b691c224c728adc658209ca
adjidedjo <adji.el.djo@gmail.com>
2020-01-29 11:38:08 +0700

    fix lucky gift

--- app/models/lucky_gift.rb
+++ app/models/lucky_gift.rb
@@ -32,7 +32,24 @@ class LuckyGift < ApplicationRecord
       end
       nilai = list_nilai.sum
     end
-    return list_kode
+    
+    if nilai < voucher.to_i
+      list_nilai2 = []
+      list_kode2 = []
+      nilai = list_nilai.last
+      nilai2 = 0
+      tv = find_by_sql("SELECT * FROM traveloka_vouchers WHERE nilai != #{list_nilai.last} AND used = false ORDER BY CAST(nilai AS DECIMAL) DESC")
+      for e in tv
+        if (nilai2+e.nilai.to_i) <= voucher.to_i
+          list_nilai2 << e.nilai.to_i
+          list_kode2 << e.id
+        end
+        nilai2 = list_nilai2.sum
+      end
+      return list_kode2
+    else
+      return list_kode
+    end
   end
 
   def self.update_traveloka_voucher(voucher)
